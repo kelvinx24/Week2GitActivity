bb41627c96d377054ed4ffa67c2ee6b760f149b9
sneaky-g <103377117+sneaky-g@users.noreply.github.com>
2024-09-13 15:02:43 -0400

    implement divide operation

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ console.log(`result_add = ${result_add}`);
 
 const result_subtract = calc.subtract(5, 2);
 console.log(`result_subtract = ${result_subtract}`);
-
 const result_multiply = calc.multiply(3, 4);
 console.log(`result_multiply = ${result_multiply}`);
 
+const result_divide = calc.divide(8, 2);
+console.log(`result_divide = ${result_divide}`);
