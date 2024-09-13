8ed58ed76eb7277d4689519464be9a48bb6d742e
Kelvin <xu.kel@northeastern.edu>
2024-09-13 15:01:28 -0400

    implement multiply operation

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,6 @@ console.log(`result_add = ${result_add}`);
 const result_subtract = calc.subtract(5, 2);
 console.log(`result_subtract = ${result_subtract}`);
 
+const result_multiply = calc.multiply(3, 4);
+console.log(`result_multiply = ${result_multiply}`);
+
